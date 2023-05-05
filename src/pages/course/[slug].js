1fea4151c36e3680c5e5495ac10073c51b03d454
João Vitor <j.olliveira98@outlook.com>
2023-05-05 16:06:12 -0300

    corrigido map keys e layout mobile para info dos cursos

--- src/pages/course/[slug].js
+++ src/pages/course/[slug].js
@@ -14,19 +14,18 @@ import Flag from "react-world-flags";
 export default function Course({ course }) {
   const router = useRouter();
   const { source } = router.query;
-  console.log(course);
 
   return (
-    <section className="container flex justify-center gap-4">
+    <section className="container flex flex-col lg:flex-row justify-center gap-8">
       {router.isFallback ? (
         <Skeleton />
       ) : (
         <>
-          <article className="md:pr-8">
-            <h3 className="text-xl font-bold mb-2 leading-snug text-zinc-900 dark:text-slate-50">
-              Sobre {course.fields.title}
-            </h3>
+          <article>
             <div className="mx-auto prose text-zinc-900 dark:text-slate-50">
+              <h3 className="text-xl font-bold mb-2 leading-snug text-zinc-900 dark:text-slate-50">
+                Sobre {course.fields.title}
+              </h3>
               <ContentfulImage
                 alt={`Cover image for ${course.fields.title}`}
                 src={course.fields.coverImage.fields.file.url}
@@ -35,9 +34,52 @@ export default function Course({ course }) {
                   course.fields.coverImage.fields.file.details.image.height
                 }
               />
+              <div className="flex flex-col sm:hidden items-center lg:hidden text-sm font-medium bg-white dark:bg-zinc-900 border-r border-t border-l border-slate-300 dark:border-zinc-800 rounded-tl-lg rounded-tr-lg">
+                <div className="w-full flex grow gap-1 px-4 py-2 border-b border-slate-300 dark:border-zinc-800">
+                  <FontAwesomeIcon icon={faStar} />
+                  Cursos Certificados
+                </div>
+                <div className="w-full flex grow gap-1 px-4 py-2 border-b">
+                  <FontAwesomeIcon icon={faExternalLink} />
+                  <Link href={course.fields.website} passHref={true}>
+                    {course.fields.website}
+                  </Link>
+                </div>
+              </div>
+              <div className="hidden sm:flex items-center lg:hidden text-sm font-medium bg-white dark:bg-zinc-900 border-r border-t border-l border-slate-300 dark:border-zinc-800 rounded-tl-lg rounded-tr-lg">
+                <div className="flex grow items-center gap-1 px-4 py-2 border-r border-slate-300 dark:border-zinc-800 ">
+                  <FontAwesomeIcon icon={faStar} />
+                  Cursos Certificados
+                </div>
+                <div className="flex grow items-center gap-1 px-4 py-2">
+                  <FontAwesomeIcon icon={faExternalLink} />
+                  <Link href={course.fields.website} passHref={true}>
+                    {course.fields.website}
+                  </Link>
+                </div>
+              </div>
+              <div className="flex items-center lg:hidden text-sm font-medium bg-white dark:bg-zinc-900 border border-slate-300 dark:border-zinc-800 rounded-bl-lg rounded-br-lg">
+                <div className="flex flex-col grow gap-1 px-4 py-2">
+                  <div className="flex gap-1">
+                    <FontAwesomeIcon icon={faTags} /> Tags
+                  </div>
+                  <div className="grid grid-cols-2 gap-1">
+                    {course.fields.tags.map((tag) => (
+                      <Link
+                        key={tag}
+                        href={`/knowledgearea/${tag}`}
+                        passHref={true}
+                      >
+                        <span className="w-full bg-slate-200 dark:bg-zinc-900 text-zinc-900 dark:text-slate-50 text-xs font-medium mr-2 px-2.5 py-0.5 rounded text-center">
+                          {String(tag).toUpperCase()}
+                        </span>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
               <RichText content={course.fields.description} />
             </div>
-            <div className="block lg:hidden pt-4 md:pt-0">{source}</div>
           </article>
           <div className="hidden lg:block py-9">
             <ul className="w-64 text-sm font-medium bg-white dark:bg-zinc-900 border border-slate-300 dark:border-zinc-800 text-zinc-900 dark:text-slate-50 rounded-lg">
@@ -64,16 +106,17 @@ export default function Course({ course }) {
                 </Link>
               </li>
               <li className="w-full flex flex-col gap-1 px-4 py-2 rounded-b-lg">
-                <div className="felx gap-1">
-                  <FontAwesomeIcon icon={faTags} /> Tags:
+                <div className="flex gap-1">
+                  <FontAwesomeIcon icon={faTags} /> Tags
                 </div>
                 <div className="grid grid-cols-2 gap-1">
                   {course.fields.tags.map((tag) => (
-                    <Link href={`/knowledgearea/${tag}`} passHref={true}>
-                      <span
-                        key={tag}
-                        class="w-full bg-slate-200 dark:bg-zinc-900 text-zinc-900 dark:text-slate-50 text-xs font-medium mr-2 px-2.5 py-0.5 rounded text-center"
-                      >
+                    <Link
+                      key={tag}
+                      href={`/knowledgearea/${tag}`}
+                      passHref={true}
+                    >
+                      <span className="w-full bg-slate-200 dark:bg-zinc-900 text-zinc-900 dark:text-slate-50 text-xs font-medium mr-2 px-2.5 py-0.5 rounded text-center">
                         {String(tag).toUpperCase()}
                       </span>
                     </Link>
